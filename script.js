bcf24ddf12b2ef6344024c54701e092360c666f3
Kamanja5 <138703488+kamanja5@users.noreply.github.com>
2024-07-21 20:52:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('#skills',{
+    strings: ["Web Developer", "Freelancer", "Web Designer"],
+    typeSpeed: 20,
+    backSpeed: 10,
+    backDelay: 2000,
+    loop: true,
+});
